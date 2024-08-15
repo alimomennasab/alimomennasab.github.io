b49138d5ed1c47f1b16559cebb2e333e0d7126b4
alimomennasab <alimomennasab3@gmail.com>
2024-08-15 11:39:01 -0700

    updated fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-amber-100 flex items-center justify-center">
+    <div className="h-screen w-screen bg-amber-50 flex items-center justify-center">
       <div className="flex flex-row justify-between p-4 w-4/6 h-4/6">
 
         {/* SIDEBAR */}
         <div className="flex flex-col w-3/10 justify-start items-end border-r border-black pr-14">
           <h1 className="text-green-600 font-bold text-3xl text-right p-1"> Ali Momennasab </h1>
-          <h1 className="text-green-600 text-xl text-right p-1"> Projects </h1>
-          <h1 className="text-green-600 text-xl text-right p-1"> Photos </h1>
-          <h1 className="text-green-600 text-xl text-right p-1"> Resume </h1>
+          <h1 className="text-green-600 font-light text-xl text-right p-1"> Projects </h1>
+          <h1 className="text-green-600 font-light text-xl text-right p-1"> Photos </h1>
+          <h1 className="text-green-600 font-light text-xl text-right p-1"> Resume </h1>
           <div className="relative w-full aspect-square mt-4 border border-black overflow-hidden">
             <Image
               src="/personal_image.png"
@@ -27,12 +27,14 @@ export default function Home() {
         <div className="flex flex-col justify-start w-3/5">
           <h1 className="text-black text-lg">
             Hi! I'm a third-year computer science major attending Cal Poly Pomona.
-            I'm interested in web development and deep learning. Check out what I've worked on in my Projects page!
+            I'm interested in web development and deep learning. Check out what I've worked on in my 
+            <span className="text-green-500"> Projects </span> 
+            page!
           </h1>
 
           {/* LINKS */}
           <h1 className="text-black text-lg mt-8">
-            Reach out anytime at <span className="font-bold">amomennasab at cpp dot edu</span>.
+            Reach out anytime at <span className="font-bold">amomennasab (at) cpp (dot) edu</span>.
           </h1>
           <h1 className="text-black text-lg"> otherwise, </h1>
           <h1 className="text-black font-bold text-2xl mt-8 mb-4">
---
+++ src/app/photos/page.tsx
@@ -0,0 +1,8 @@
+export default function PhotosPage() {
+    return (
+      <div>
+        <h1>Photos</h1>
+      </div>
+    );
+  }
+  
