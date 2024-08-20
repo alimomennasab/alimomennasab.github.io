7e1ee28b5a3a8734f0bf8c0456b612ca80d65eb0
alimomennasab <alimomennasab3@gmail.com>
2024-08-20 13:06:18 -0700

    responsive sizing for main page and sidebar

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -7,7 +7,7 @@ type SidebarProps = {
 
 const Sidebar: React.FC<SidebarProps> = ({ imageUrl }) => {
   return (
-    <div className="flex flex-col w-3/10 justify-start items-end border-r border-black pr-14">
+    <div className="flex flex-col w-3/10 justify-start items-end md:border-r sm:border-r-0 border-black pr-14">
       <h1 className="text-green-600 font-bold text-3xl text-right p-1">
         <Link href="/" className="text-green-600 hover:font-bold">
           Ali Momennasab
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-/* eslint-disable react/no-unescaped-entities */
 import Link from 'next/link';
 import Sidebar from './components/sidebar';
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-amber-50 flex items-center justify-center">
-      <div className="bg bg-amber-50 flex flex-row justify-between p-4 w-4/6 h-4/6">
-
+    <div className="min-h-screen w-screen bg-amber-50 flex items-center justify-center p-4">
+      <div className="bg-amber-50 flex flex-col md:flex-row justify-between w-full max-w-4xl">
         {/* SIDEBAR */}
         <Sidebar imageUrl="/images/personal_image.png" />
-
         {/* MAIN TEXT */}
-        <div className="flex flex-col justify-start w-3/5">
-          <h1 className="text-black text-lg">
+        <div className="flex flex-col justify-start w-full md:w-3/5 mt-8 md:mt-0">
+          <h1 className="text-black text-base md:text-lg">
             Hi! I'm a third-year computer science major attending Cal Poly Pomona.
             I'm interested in web development and deep learning. Check out what I've worked on in my
             <span>&nbsp;</span>
@@ -24,49 +21,46 @@ export default function Home() {
             <span>&nbsp;</span>
             page!
           </h1>
-
           {/* LINKS */}
-          <h1 className="text-black text-lg mt-8">
+          <h1 className="text-black text-base md:text-lg mt-6 md:mt-8">
             Reach out anytime at <span className="font-bold">amomennasab (at) cpp (dot) edu</span>.
           </h1>
-          <h1 className="text-black text-lg"> otherwise, </h1>
-          <h1 className="text-black font-bold text-2xl mt-8 mb-4">
+          <h1 className="text-black text-base md:text-lg"> otherwise, </h1>
+          <h1 className="text-black font-bold text-xl md:text-2xl mt-6 md:mt-8 mb-3 md:mb-4">
             Find Me @
           </h1>
-          <div className="flex flex-row justify-between w-1/2 mb-4">
-            <a 
-              href="https://github.com/alimomennasab" 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              className="text-green-600 text-lg hover:underline"
+          <div className="flex flex-row justify-between w-full md:w-1/2 mb-3 md:mb-4">
+            <a
+              href="https://github.com/alimomennasab"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-600 text-base md:text-lg hover:underline"
             >
               Github
             </a>
-            <a 
-              href="https://www.linkedin.com/in/amomennasab/" 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              className="text-green-600 text-lg hover:underline"
+            <a
+              href="https://www.linkedin.com/in/amomennasab/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-600 text-base md:text-lg hover:underline"
             >
               Linkedin
             </a>
-            <a 
-              href="https://x.com/momennasabali" 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              className="text-green-600 text-lg hover:underline"
+            <a
+              href="https://x.com/momennasabali"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-600 text-base md:text-lg hover:underline"
             >
               Twitter
             </a>
           </div>
-
           {/* CURRENTLY */}
-          <h1 className="text-black font-bold text-2xl mt-4 mb-4">
+          <h1 className="text-black font-bold text-xl md:text-2xl mt-3 md:mt-4 mb-3 md:mb-4">
             Currently
           </h1>
-          <h1 className="text-black text-lg"> - researching at the Cardiac Vision Lab at UCSF </h1>
-          <h1 className="text-black text-lg"> - looking for software engineering/deep learning roles for Fall 2024, Winter 2024/25, and Spring 2025</h1>
-
+          <h1 className="text-black text-base md:text-lg"> - researching at the Cardiac Vision Lab at UCSF </h1>
+          <h1 className="text-black text-base md:text-lg"> - looking for software engineering/deep learning roles for Fall 2024, Winter 2024/25, and Spring 2025</h1>
         </div>
       </div>
     </div>
